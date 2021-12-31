9479eb2850a36e8887b3e1741904310ad6ac040e
SpideyJr <95563193+SpideyJr@users.noreply.github.com>
2021-12-31 19:44:27 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('SpideyJr.', 'Credits: https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
